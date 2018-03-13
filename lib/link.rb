43806f1cbdeec37f5b40333a8c650287cef287ea
Charlene Bastians <charlenebastians@hotmail.com>
2018-03-13 11:36:22 +0000

    Set up test env

--- lib/link.rb
+++ lib/link.rb
@@ -1,10 +1,14 @@
+require 'pg'
+
 class Link
 
   def self.all
-    [
-      "http://www.makersacademy.com",
-      "http://www.google.com",
-      "http://www.twitter.com"
-    ]
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+    else
+      connection = PG.connect(dbname: 'bookmark_manager')
+    end
+    result = connection.exec('SELECT * FROM links')
+    result.map { |link| link['url']}
   end
 end
--- spec/features/view_links_spec.rb
+++ spec/features/view_links_spec.rb
@@ -1,7 +1,6 @@
 feature 'testing links can be viewed' do
   scenario 'when requested by user, display inks' do
     visit '/'
-    click_lick 'View'
     expect(page).to have_content 'www.makersacademy.com'
     expect(page).to have_content 'www.google.com'
     expect(page).to have_content 'www.twitter.com'
